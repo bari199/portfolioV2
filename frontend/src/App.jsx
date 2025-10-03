e135479bf9fa2048ef16dbf481dd87bc00c319e9
Samim441 <mn3905827@gmail.com>
2025-10-03 19:29:32 +0530

    added About files

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
-import Navbar from './components/Navbar/navbar'
-import Hero from './components/Hero/Hero'
-
+import React from "react";
+import Navbar from "./components/Navbar/navbar";
+import Hero from "./components/Hero/Hero";
+import About from "./components/About/About";
 
 const App = () => {
   return (
     <div>
-      <Navbar/>
-      <Hero/>
+      <Navbar />
+      <Hero />
+      <About />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
