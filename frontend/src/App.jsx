35383ff635a828e6e3dfe4b71b2d43c0ce245d33
Samim441 <mn3905827@gmail.com>
2025-10-04 00:18:52 +0530

    modify service pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Navbar from "./components/Navbar/navbar";
 import Hero from "./components/Hero/Hero";
 import About from "./components/About/About";
+import Services from "./components/Services/Services";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
@@ -11,6 +12,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Services/>
       <Contact />
       <Footer/>
     </div>
