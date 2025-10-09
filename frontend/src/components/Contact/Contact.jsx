200913210aa12d130160fb93efdf5976b998e332
bari@199 <bari1999smab@gmail.com>
2025-10-09 08:40:52 -0700

    fixes case sensitive css import path

--- frontend/src/components/Contact/Contact.jsx
+++ frontend/src/components/Contact/Contact.jsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React,{useState} from "react";
 import "./Contact.css";
 import theme_pattern from "../../assets/theme_pattern.svg";
 import mail_icon from "../../assets/mail_icon.svg";
 import location_icon from "../../assets/location_icon.svg";
 import call_icon from "../../assets/call_icon.svg";
-import { useState } from "react";
+
 
 const Contact = () => {
 
