22e52d6c1e7ab2dc578909f3eb11ed37b3e990db
bari@199 <bari1999smab@gmail.com>
2025-10-09 07:48:07 -0700

    fixes case sensitive css import path

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import './Footer.css'
-import footer_logo from '../../assets/footer_logo.svg'
 import theme_pattern from '../../assets/theme_pattern.svg'
 import user_icon from "../../assets/user_icon.svg";
 const Footer = () => {
